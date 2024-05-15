a9b48784aa537188a56c9fdb4797d04aaf80085f
AlexanderRios <1443935@senati.pe>
2024-05-15 17:42:22 -0500

    nuevo p2

---
+++ p2.php
@@ -0,0 +1,9 @@
+<?php
+$nombres="gandy william";
+$apellido="humiri quispe";
+$curso="BACKEND DEVELOPER WEB";
+$semestre = "ciclo 3";
+//juntar nombres y apellidos curso y semestre
+echo $nombres." ".$apelidos." ".$curso." ".$semestre ;
+
+?>
