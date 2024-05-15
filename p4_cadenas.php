3e503e1d1bb333358256bff39d90e83345ae10b0
AlexanderRios <1443935@senati.pe>
2024-05-15 17:48:32 -0500

    nuevas cadenas

---
+++ p4_cadenas.php
@@ -0,0 +1,11 @@
+<?php
+    $cadena= "el curso a terminado";
+
+    echo " el texto en mayuscula ".strtoupper($cadena)."<br>";
+    echo " el texto en ,iniscula ".strtoupper($cadena)."<br>";
+    echo " el texto tiene ".strlen($cadena)." caracteres<br>";
+    echo " el texto tiene ".
+  str_replace( "terminado","finalizado",$cadena)."<br>";
+  echo "</h1>";
+  ?>
+
