f43a83d72ffc3f070bd0ecf506ea7b8eb2591989
AlexanderRios <1443935@senati.pe>
2024-05-15 17:50:08 -0500

    nuevas operaciones

---
+++ p3_operaciones.php
@@ -0,0 +1,13 @@
+<?php
+$numero1=10;
+$numero2=20;
+
+$suma = $numero1 + $numero2;
+$resta = $numero1 - $numero2;
+$multiplicacion = $numero1 * $numero2;
+$division = $numero1 / $numero2;
+echo "la suma es ".$suma."<br>";
+echo "la resta es ".$resta."<br>";
+echo "la multiplicacion es ".$multiplicacion."<br>";
+echo "la divicion es ".$division."<br>";
+
