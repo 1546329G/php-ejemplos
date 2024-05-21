9557949b305261cfde8eef665413f37b1b622f81
AlexanderRios <1443935@senati.pe>
2024-05-21 17:16:43 -0500

    calculadora

---
+++ p12_clases.php
@@ -0,0 +1,55 @@
+<?php
+
+class calculadora {   
+
+   public $primerNumero;
+   public $segundoNumero;
+
+   function __construct($num1 = 0, $num2 = 0) {
+   $this->primerNumero=$num1;
+   $this->segundoNumero=$num2;
+}
+    
+   public function sumar() {
+      return $this->primerNumero+$this->segundoNumero;
+   }
+
+
+    public function restar (){
+     return $this->primerNumero-$this->segundoNumero;
+        
+    }
+
+
+    public function multiplicar (){
+     return $this->primerNumero * $this->segundoNumero;
+        
+    }
+
+
+    public function dividir (){
+    return $this->primerNumero / $this->segundoNumero;
+        
+    }
+
+
+    public function potencia (){
+       return pow($this->primerNumero, $this->segundoNumero);
+            
+    
+    }
+
+    public function raiz (){
+       return sqrt($this->primerNumero, $this->segundoNumero);
+         
+        
+    }
+ 
+
+   
+  
+    
+    
+
+
+}
