73c68a967cbf29c303d97c71ec0e1ca4723ae8cb
Alexander <1546329@senati.pe>
2024-06-05 17:56:37 -0500

    ADD:CAMBIOS

--- p25.1_ajax.js
+++ p25.1_ajax.js
@@ -6,14 +6,39 @@ function BuscarPacientes(){
         nombre : $nombre
 
      };
+
     $.ajax({
         url :"p26_ajax_pdo.php",
         type : "post",
         data :datos,
         succes : function(result){
-            console.log(result);
+            debugger;
+             //console.log(result);
+             const pacientes = result;
+             pacientes.forEach(item => {
+             agregarfilas("#tabla", iten);
+        });
 
         }
     })
     return;
 }
+        function agregarfilas (id, paciente) {
+            const html =
+            "<tr>"+
+            "<td>"+paciente. Nombres+"</td>"+
+            "<td>"+paciente.Edad+"</td>"+
+            "<td>"+paciente.talla+"</td>"+
+            "<td>"+paciente.peso+"</td>"+
+            "<td>"+paciente.Tos+"</td>"+
+            "<td>"+paciente.Fiebre+"</td>"+
+            "<td>"+paciente.Disnea+"</td>"+
+            "<td><button type='button'>Editar</></td>"+
+            "</tr>";
+            $(id +" tr:last").after(html);
+    }
+
+
+
+
+
