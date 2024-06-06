8e24702ba0c0caea8b2c8d8cd5f64d2e9b11c4d4
1546329G <1546329@senati.pe>
2024-06-06 16:33:32 -0500

    nuevo modififcacion

--- P22.1_consulta_pdo.php
+++ P22.1_consulta_pdo.php
@@ -9,7 +9,11 @@
     <center><h1>Busqueda de Pacientes</h1></center>
     <form action="p22_lista_pdo.php" method="GET">
         <input type="text" name="search_query" placeholder="Escribe aquí">
-        <button type="submit">Buscar</button>
+       <!--<button type="submit">Buscar</button>-->
+       <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+       Buscar
+        </button>
+
     </form>
 </body>
 </html>
--- p25.1_ajax.js
+++ p25.1_ajax.js
@@ -1,44 +1,40 @@
-function BuscarPacientes(){
-    const nombre =  $("#nombre").val();
-    //alert("buscando..."+nombre0);
-
-     let datos ={
+function buscarPacientes() {
+    const $nombre = $("#nombre").val();
+    //alert("Buscando..."+nombre);
+    let datos ={
         nombre : $nombre
-
-     };
-
+    };
     $.ajax({
         url :"p26_ajax_pdo.php",
         type : "post",
-        data :datos,
-        succes : function(result){
+        data : datos,
+        success : function(result) {
             debugger;
-             //console.log(result);
-             const pacientes = result;
-             pacientes.forEach(item => {
-             agregarfilas("#tabla", iten);
-        });
+            console.log(result);
+            const pacientes=$.parseJSON(result);
+            pacientes.forEach(item => {
+                agregarFilas("#tabla",item);
+            });
 
         }
     })
+
+
     return;
 }
-        function agregarfilas (id, paciente) {
-            const html =
-            "<tr>"+
-            "<td>"+paciente. Nombres+"</td>"+
-            "<td>"+paciente.Edad+"</td>"+
-            "<td>"+paciente.talla+"</td>"+
-            "<td>"+paciente.peso+"</td>"+
-            "<td>"+paciente.Tos+"</td>"+
-            "<td>"+paciente.Fiebre+"</td>"+
-            "<td>"+paciente.Disnea+"</td>"+
-            "<td><button type='button'>Editar</></td>"+
-            "</tr>";
-            $(id +" tr:last").after(html);
-    }
 
 
-
-
-
+function agregarFilas(id,paciente) {
+    const html=
+    "<tr>"+
+    "<td>"+paciente.nombres+"</td>"+
+    "<td>"+paciente.edad+"</td>"+    
+    "<td>"+paciente.talla_m+"</td>"+
+    "<td>"+paciente.peso_kg+"</td>"+
+    "<td>"+paciente.sintoma_tos+"</td>"+
+    "<td>"+paciente.sintoma_fiebre+"</td>"+
+    "<td>"+paciente.sintoma_disnea+"</td>"+
+    "<td><button type='button'>Editar</button></td>"+
+    "</tr>";
+    $(id+" tr:last").after(html);
+}
